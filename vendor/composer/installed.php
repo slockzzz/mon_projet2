5fd96a78f668a25d883dfc17797f706c8b5b5e4c
slockzzz <fourdrainclement02@gmail.com>
2025-04-01 17:29:47 +0200

    Ajout du workflow pour PHPCS

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '7b509f6974042c667bdd95d74edd37465100e07f',
+        'reference' => '14e49f2e14a90392ea9d7cd11b043f9524c1af1f',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '7b509f6974042c667bdd95d74edd37465100e07f',
+            'reference' => '14e49f2e14a90392ea9d7cd11b043f9524c1af1f',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
